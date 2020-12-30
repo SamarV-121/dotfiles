6603f8b8da637d365814507c92e0340fd1897f69
SamarV-121 <samarvispute121@gmail.com>
2020-12-30 21:44:35 +0530

    build.sh: Add option to save config
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- bin/build.sh
+++ bin/build.sh
@@ -46,65 +46,82 @@ export KBUILD_BUILD_HOST=ThePASIV
 export KBUILD_BUILD_USER=SamarV-121
 export LINEAGE_VERSION_APPEND_TIME_OF_DAY=true
 
-# GAPPS
-echo -e "$BOLD"Press Y to include GAPPS in build.
-read -t5 -r GAPPS
+echo -e "$BOLD"Press Y to setup new config
+read -t10 -r CONF
+
+if [[ $CONF != Y && -e .config ]]; then
+	eval "$(cat .config)"
+else
+	[[ ! -e .config ]] && echo -e "$BOLD"No config file has been found, setup new
+	rm -f .config
+	echo -e "$BOLD"Press Y to include GAPPS in build
+	read -t5 -r GAPPS
+	echo "export GAPPS=$GAPPS" >>.config
+
+	echo -e "$BOLD"Press Y to upload build
+	read -t5 -r UPLOAD
+	echo "export UPLOAD=$UPLOAD" >>.config
+
+	echo -e "$BOLD"Press Y to nuke outdir
+	read -t5 -r CLEAN
+	echo "export CLEAN=$CLEAN" >>.config
+
+	if [[ $CLEAN != Y ]]; then
+		echo -e "$BOLD"Press N to not nuke images and staging dirs
+		read -t5 -r INSTALLCLEAN
+		echo "export INSTALLCLEAN=$INSTALLCLEAN" >>.config
+	fi
+
+	# https://github.com/SamarV-121/android_vendor_extra/blob/lineage-18.1/product.mk#7
+	echo -e "$BOLD"Press N to disable DEBUGGING
+	read -t5 -r DEBUGGING
+	echo "export DEBUGGING=$DEBUGGING" >>.config
+
+	echo -e "$BOLD"Press N to disable CCACHE
+	read -t5 -r CCACHE
+	echo "export CCACHE=$CCACHE" >>.config
+
+	echo -e "$BOLD"Press C to clear CCACHE
+	read -t5 -r CCACHE_
+	echo "export CCACHE_=$CCACHE_" >>.config
+
+	echo -e """$BOLD""Type lunch command (default: lineage)"
+	read -t10 -r ROM_VENDOR
+	echo "export ROM_VENDOR=$ROM_VENDOR" >>.config
+
+	echo -e """$BOLD""Type build type (default: userdebug)"
+	read -t10 -r BUILD_TYPE
+	echo "export BUILD_TYPE=$BUILD_TYPE" >>.config
+
+	echo -e """$BOLD""Type target command (default: bacon)"
+	read -t10 -r TARGET_CMD
+	echo "export TARGET_CMD=$TARGET_CMD" >>.config
+
+	# DEVICE CODENAME(S)
+	if [[ "$*" ]]; then
+		DEVICES="$*"
+	else
+		echo -e """$BOLD""Enter device name(s)"
+		read -r DEVICES
+	fi
+	echo "export DEVICES=$DEVICES" >>.config
+fi
+
 [[ $GAPPS == Y ]] && export WITH_GAPPS=true
-
-# UPLOAD
-echo -e """$BOLD""Type Y to upload build (default: No)"
-read -t5 -r UPLOAD
-
-# NUKE OUT
-echo -e "$BOLD"Press Y to nuke outdir
-read -t5 -r CLEAN
 [[ $CLEAN == Y ]] && rm -rf out
-
-# Nuke images and staging dirs
-[[ $CLEAN != Y ]] && echo -e "$BOLD"Press N to not nuke images and staging dirs; read -t5 -r INSTALLCLEAN
-
-# DEBUGGING (https://github.com/SamarV-121/android_vendor_extra/blob/lineage-18.0/product.mk#7)
-echo -e "$BOLD"Press N to disable DEBUGGING
-read -t5 -r DEBUGGING
 [[ $DEBUGGING != N ]] && export WITH_DEBUGGING=true
-
-# CCACHE
-echo -e "$BOLD"Press N to disable CCACHE
-read -t5 -r CCACHE
 if [[ $CCACHE != N ]]; then
 	export USE_CCACHE=1
 	export CCACHE_EXEC=/usr/bin/ccache
 	ccache -M 50G >/dev/null
 	ccache -o compression=true
 fi
-echo -e "$BOLD"Press C to clear CCACHE
-read -t5 -r CCACHE
-[[ $CCACHE == C ]] && ccache -C
-
-# ROM VENDOR
-echo -e """$BOLD""Type lunch command (default: lineage)"
-read -t10 -r ROM_VENDOR
+[[ $CCACHE_ == C ]] && ccache -C
 [[ -z $ROM_VENDOR ]] && ROM_VENDOR=lineage
-
-# BUILD TYPE
-echo -e """$BOLD""Choose build type (default: userdebug)"
-read -t10 -r BUILD_TYPE
 [[ -z $BUILD_TYPE ]] && BUILD_TYPE=userdebug
-
-# TARGET COMMAND
-echo -e """$BOLD""Type target command (default: bacon)"
-read -t10 -r TARGET_CMD
 [[ -z $TARGET_CMD ]] && TARGET_CMD=bacon
 
 source build/envsetup.sh
-
-# DEVICE CODENAME(S)
-if [[ "$*" ]]; then
-	DEVICES="$*"
-else
-	echo -e """$BOLD""Enter device name(s)"
-	read -r DEVICES
-fi
 
 unset -f echo
 
@@ -113,15 +130,15 @@ BUILD_START=$(date +"%s")
 COUNT_DEVICES=$(echo "$DEVICES" | wc -w)
 while [ "$COUNT_DEVICES" -gt 0 ]; do
 	DEVICE=$(tac -s' ' <<<"$DEVICES" | xargs | cut -d " " -f"$COUNT_DEVICES")
-	lunch "${ROM_VENDOR}"_"$DEVICE"-"${BUILD_TYPE}"
+	lunch "${ROM_VENDOR}"_"$DEVICE"-"$BUILD_TYPE"
 	OUTDIR="$OUT"
 	append_dt_commit
 	[[ $SPAM_TELEGRAM == Y ]] && curl -s "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT" -d "parse_mode=Markdown" -d "text=Build started for $DEVICE: [See Progress](${BUILD_URL}console)
 ROM\_VENDOR: $ROM_VENDOR
 BUILD\_TYPE: $BUILD_TYPE" >/dev/null
-	rm "$OUT"/*zip* out/.lock
+	rm -f "$OUT"/*zip* out/.lock
 	[[ $INSTALLCLEAN != N ]] && m installclean
-	m "$TARGET_CMD" -j"$(nproc --all)" 2>&1 | tee build_"$DEVICE".log
+	make "$TARGET_CMD" -j"$(nproc --all)" 2>&1 | tee build_"$DEVICE".log
 	BUILD_END=$(date +"%s")
 	BUILD_DIFF=$((BUILD_END - BUILD_START))
 	OTA_PATH=$(find "$OUTDIR"/*2020*.zip)
